5a18fc04a6aacc5e8119b23acdbf7cc3aa2c2e0c
Abhay Singh Gill <abhaygill017@gmail.com>
2025-07-20 22:30:41 +0530

    SystemUI: Allow to change brightness slider positioning in new compose QS
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I57a55c924be7e36d969af4382400e8f0d9415a84
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -329,6 +329,7 @@ public class SecureSettings {
         Settings.Secure.IDENTITY_CHECK_NOTIFICATION_VIEW_DETAILS_CLICKED,
         Settings.Secure.NOTIFICATION_BUNDLES_ALWAYS_EXPAND,
         Settings.Secure.DOZE_TAP_GESTURE_AMBIENT,
-        Settings.Secure.DOZE_FOR_NOTIFICATIONS
+        Settings.Secure.DOZE_FOR_NOTIFICATIONS,
+        Settings.Secure.QS_SHOW_BRIGHTNESS_SLIDER
     };
 }
